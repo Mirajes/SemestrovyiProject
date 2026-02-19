b38cee21d82cd2633ce1be89e7f3a30f4196145d
Mirajes <myrrrclone@gmail.com>
2026-02-19 21:56:35 +0300

    дальше

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,59 +1,39 @@
-using System;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    private int _cycleCapacity = 4;
-    [SerializeField] private List<ItemType> _cycleInv = new();
 
-
-    private Dictionary<Item, GameObject> _sourceList = new();
-    [SerializeField] private List<GameObject> _interactionObjs = new();
-
-    [SerializeField] private Transform _spawnSpot;
-
-    private bool _isSpawned = false;
-    private bool _isAFK = false;
-
-    private void SuddenlyRoll()
-    {
-
-    }
-
-    private void SpawnObj(ItemType item)
-    {
-
-    }
-
-    private void OnEnable()
-    {
-        
-    }
 }
 
-public class Item : ScriptableObject
+public class ResourceData : ScriptableObject
 {
-    private string _name;
-    private int _id;
 
-    public string Name => _name;
-    public int ID => _id;
 }
 
-enum ItemType
+public class InteractableObject
 {
-    None,
-    Rock,
-    Wood,
-    Eye
+
 }
 
-enum Status
+public class StateManager
 {
-    Dialogue,
-    Minigame,
-    Home,
-    OnWay,
-    Afk,
+
 }
+
+public class CursorManager // для Raycast
+{
+
+}
+
+public class AFKHandler { } // когда игра закрыта
+
+public class Inventory { } // 
+
+public class Cycle { } // 
+
+public class GUI : MonoBehaviour
+{
+
+}
+
+public class NPC { }
--- Assets/Scripts/Interfaces.cs
+++
@@ -1,8 +0,0 @@
-public interface IAlive
-{
-    float MaxHealth { get; }
-    float CurrentHealth { get; }
-
-    void TakeDamage(float amount);
-    void HealYourself(float amount);
-}
