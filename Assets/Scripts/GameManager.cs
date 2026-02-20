14e855a72e99e226bc5ba23142878c3c15050d1c
Mirajes <myrrrclone@gmail.com>
2026-02-20 12:11:23 +0300

    продолжаем

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,38 +2,77 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    [Header("Player")]
+    [SerializeField] private GameObject _playerPrefab;
+    [SerializeField] private Transform _playerPos;
 
+    [Header("Entity")]
+    [SerializeField] private Transform _entitySpawnPos;
+    private Entity _currentEntity;
+    private float _entitySpawnDelay = 0.5f;
+
+    
 }
+
 
 public class ResourceData : ScriptableObject
 {
+    [SerializeField] private string _name;
+    [SerializeField] private Sprite _sprite;
+    [SerializeField] private int _count;
 
+    [SerializeField] private int _index;
+
+    [SerializeField] private ResourceData _reverseResource; // shifter, skinwalker
+
+    public string Name => _name;
+    public Sprite Sprite => _sprite;
+    public int Count => _count;
+    public int Index => _index;
 }
 
-public class InteractableObject
+public class Inventory
 {
 
 }
 
-public class StateManager
+public class ItemSlot : MonoBehaviour
+{
+    private ResourceData _resource;
+
+
+    public ResourceData Resource => _resource;
+
+    private void OnMouseUpAsButton()
+    {
+        
+    }
+}
+public class CursorManager : MonoBehaviour // для Raycast
+{
+    
+}
+
+public class Entity
 {
 
 }
 
-public class CursorManager // для Raycast
-{
+//public class StateManager
+//{
 
-}
+//}
 
-public class AFKHandler { } // когда игра закрыта
 
-public class Inventory { } // 
+//public class AFKHandler { } // когда игра закрыта
 
-public class Cycle { } // 
+//public class Inventory { } // 
 
-public class GUI : MonoBehaviour
-{
+//public class Cycle { } // 
 
-}
+//public class GUI : MonoBehaviour
+//{
 
-public class NPC { }
+//}
+
+//public class NPC { }
---
+++ Assets/Scripts/SaveTest.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using System.Collections.Generic;
+
+[Serializable]
+public class NPC_SaveTest : MonoBehaviour
+{
+    [SerializeField] private int _hp;
+    [SerializeField] private string _name;
+
+    
+}
+
+public class SaveManagerTest : MonoBehaviour
+{
+    private Dictionary<string, object> _saveData = new();
+
+    public void Save()
+    {
+        //var saveObjs = GameObject.FindAnyObjectByType<>
+    }
+}
+public interface ISavable
+{
+    public object SaveData();
+    //public 
+}
